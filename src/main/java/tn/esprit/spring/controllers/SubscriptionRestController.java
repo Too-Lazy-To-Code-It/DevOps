b5f5a0c20a49d88e59f6b9bb7f4c3fd0492d2c68
rayenalmi <rayenalmi64@gmail.com>
2024-11-05 10:10:07 +0100

    add subsc work and chaneg image  tag:3

--- src/main/java/tn/esprit/spring/controllers/SubscriptionRestController.java
+++ src/main/java/tn/esprit/spring/controllers/SubscriptionRestController.java
@@ -52,7 +52,7 @@ public class SubscriptionRestController {
 
 
     @Operation(description = "Retrieve All Subscriptions")
-    @GetMapping("/all")
+    @GetMapping("/all/subscriptions")
     public List<Subscription> getAllSubscription(){
         return subscriptionServices.retrieveSubscriptionsAll();
     }
