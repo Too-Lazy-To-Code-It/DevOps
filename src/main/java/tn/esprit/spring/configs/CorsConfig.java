bf8c5990dcef561d36067ef4c99b1ec0f6dd4110
rayenalmi <rayenalmi64@gmail.com>
2024-11-06 12:12:28 +0100

    file config cors

--- src/main/java/tn/esprit/spring/configs/CorsConfig.java
+++ src/main/java/tn/esprit/spring/configs/CorsConfig.java
@@ -13,10 +13,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://192.168.8.104:3001")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
     }
